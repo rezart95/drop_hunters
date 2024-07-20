c0c053aee5e248c4fb1c1e1b473849ed5ad7e7a1
Rezart Abazi <rezart392@gmail.com>
2024-07-20 16:41:10 +0200

    add some dash viz

---
+++ app.py
@@ -0,0 +1,11 @@
+from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
+import layout
+
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.QUARTZ])
+
+app.layout = layout.layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8050)
