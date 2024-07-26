50cdfde45976a9d63304955ed66a1f666ed8b7d2
Rezart Abazi <rezart392@gmail.com>
2024-07-26 14:27:13 +0200

    add product url

--- webscraping.py
+++ webscraping.py
@@ -20,35 +20,41 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
     
     # Find the relevant elements containing the data
-    products_title = [title.text.strip() for title in soup.find_all('div', class_='product-tile__info')]
     products_title = [title.text.strip()[:-11] for title in soup.find_all('div', class_='product-tile__info')]
     products_price = [price.text.strip() for price in soup.find_all('div', class_='product-tile__info__price')]
     product_images = []
+    product_urls = []
+    
     for picture in soup.find_all('picture'):
         source_tag = picture.find('source', media="(max-width: 600px)")
         if source_tag:
             product_images.append(source_tag['srcset'].split(', ')[0].split(' ')[0])
+            # Find the parent <a> tag to get the product URL
+            parent_a_tag = picture.find_parent('a', {'data-track': 'productTile'})
+            if parent_a_tag:
+                product_urls.append(f"https://chylak.com{parent_a_tag['href']}")
     product_stock = [stock.text.strip() for stock in soup.find_all('div', class_='product-tile__stock')]
-
-    print("PRODUCT STOCK", product_stock) # not all the products have stock information
 
     # Extract every second URL (starting from the second one)
     product_only_image_urls = product_images[::2]
+    product_only_one_url = product_urls[::2]
 
     # Create a DataFrame from the data
     data = {
         'Product Title': products_title,
         'Product Price': products_price,
         'Product Image': product_only_image_urls,
+        'Product URL': product_only_one_url,
         # 'Product Stock': product_stock
     }
     df = pd.DataFrame(data)
 
     # Print the DataFrame
     pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.max_columns', None)
+    print(df)
 else:
     print(f"Failed to retrieve the web page. Status code: {response.status_code}")
-
 
 
 # Folder where images will be saved
